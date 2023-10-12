3b437a3e4c16ddca91b8daa4e6037891c0f563b5
hrodriguezimagina <139818140+hrodriguezimagina@users.noreply.github.com>
2023-10-12 16:57:37 -0500

    - added type : text, color and media (#24)
    * - added type : text, color and media
    
    * - added callback redirect after create
    
    * - fix validate id
    
    * - fix required field is not sent from brackend when is false
    
    * - fix : required toggle
    
    ---------
    
    Co-authored-by: leo-rp <leorock1988@hotmail.com>

--- _crud/productOptions.vue
+++ _crud/productOptions.vue
@@ -16,6 +16,11 @@ export default {
         extraFormFields: 'Icommerce.crud-fields.productOptions',
         create: {
           title: this.$tr('icommerce.cms.newOption'),
+          callback: (model) => {
+            if(model?.id){
+              this.$router.push({name: 'qcommerce.admin.options.edit', params: {id: model.id }})
+            }
+          }
         },
         read: {
           columns: [
