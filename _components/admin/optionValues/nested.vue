889618a593d43b81f2f1b59fb256546488ce9427
SneiderDeveloper <37787288+SneiderDeveloper@users.noreply.github.com>
2024-02-14 15:05:20 -0500

    Updating the draggable component from vuedraggable. Now, a slot is required instead of v-for

--- _components/admin/optionValues/nested.vue
+++ _components/admin/optionValues/nested.vue
@@ -2,41 +2,44 @@
   <div>
     <draggable
       tag="div"
-      v-if="optionValues.length>0"
+      v-if="optionValues.length > 0"
       v-bind="dragOptions"
       :list="optionValues"
-      :group="{ name: 'g1' }">
-      <div
-        class="list-group-item"
-        v-for="(optionValue, index) in optionValues"
-        :key="optionValue.id">
-        <div class="row justify-between q-mb-xs q-mt-xs">
-          <div class="col-6 q-py-xs">
-            <q-icon class="cursor-pointer" name="fas fa-arrows-alt"/>
-            {{optionValue.description}}
+      :group="{ name: 'g1' }"
+      item-key="name"
+    >
+      <template #item="{ optionValue }">
+        <div
+          class="list-group-item"
+          :key="optionValue.id">
+          <div class="row justify-between q-mb-xs q-mt-xs">
+            <div class="col-6 q-py-xs">
+              <q-icon class="cursor-pointer" name="fas fa-arrows-alt"/>
+              {{optionValue.description}}
+            </div>
+            <div class="col-6 text-right q-py-xs relative-position">
+              <!--:to="{name: 'qcommerce.admin.optionValues.update', params: {optionId: $route.params.id, id: optionValue.id}}"-->
+              <q-btn
+                @click="$root.$emit('showEdit',optionValue.id)"
+                icon="fas fa-pen"
+                size="xs"
+                class="q-mr-sm"
+                color="green"/>
+              <q-btn
+                @click="dialogDeleteItem = true; itemIdToDelete = optionValue"
+                icon="fas fa-trash-alt"
+                size="xs"
+                color="red"/>
+            </div>
+            <div class="col-12">
+              <q-separator/>
+            </div>
           </div>
-          <div class="col-6 text-right q-py-xs relative-position">
-            <!--:to="{name: 'qcommerce.admin.optionValues.update', params: {optionId: $route.params.id, id: optionValue.id}}"-->
-            <q-btn
-              @click="$root.$emit('showEdit',optionValue.id)"
-              icon="fas fa-pen"
-              size="xs"
-              class="q-mr-sm"
-              color="green"/>
-            <q-btn
-              @click="dialogDeleteItem = true; itemIdToDelete = optionValue"
-              icon="fas fa-trash-alt"
-              size="xs"
-              color="red"/>
-          </div>
-          <div class="col-12">
-            <q-separator/>
-          </div>
+          <nestedDraggable v-if="optionValue.children"
+                           :class="`${optionValue.children.length} ?: q-mb-xs`"
+                           :optionValues="optionValue.children"/>
         </div>
-        <nestedDraggable v-if="optionValue.children"
-          :class="`${optionValue.children.length} ?: q-mb-xs`"
-          :optionValues="optionValue.children"/>
-      </div>
+      </template>
     </draggable>
     <div class="col-12 text-center text-h5" v-else>
       <not-result />
