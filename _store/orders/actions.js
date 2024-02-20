d6d273768464dcc31fefd2d01d7bbc6a4f8f288e
SneiderDeveloper <37787288+SneiderDeveloper@users.noreply.github.com>
2024-02-19 19:13:44 -0500

    Update eventBus usage in nested.vue and actions.js

--- _store/orders/actions.js
+++ _store/orders/actions.js
@@ -1,12 +1,13 @@
 import eCommerceService from 'modules/qcommerce/_services/index'
+import { eventBus } from 'src/plugins/utils'
 
 export const GET_ORDERS_PENDING = ({ commit, state, dispatch }, Vue) => {
   return new Promise(async (resolve, reject) => {
     let params = { refresh: true, params: { filter: { status: 13 } } }
     eCommerceService.crud.index('apiRoutes.qcommerce.orders', params).then(response => {
       commit('SET_ORDERS_PENDING', response.data)
-      Vue.$root.$emit('new-order')
-      Vue.$root.$emit('orders-updated')
+      eventBus.emit('new-order')
+      eventBus.root.$emit('orders-updated')
       resolve(true)
     }).catch(error => {
       console.error('[GET ORDERS ACTION]', error)
