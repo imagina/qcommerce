94dbe730c51076f6ba4622bdd52f51ff2f3ea51f
msolanorenuity <msolano@contractor.renuityhome.com>
2025-05-09 11:11:54 -0500

    Add category display to order items in order view
    Enhanced the order details page to show the category title for each order item. Updated the API request to include category data for order items, and modified the table to display this information in a new column.

--- _pages/admin/order/show.vue
+++ _pages/admin/order/show.vue
@@ -34,6 +34,15 @@
       <!--Product list-->
       <q-table v-bind="tableConfig.props">
         <!-- Custom column -->
+        <!-- Custom column -->
+        <template v-slot:body-cell-title="props">
+          <q-td :props="props" :title="props.value">
+            {{ props.value }}
+            <div v-if="props.row.category" class="text-blue-grey">
+              {{props.row.category.title}}
+            </div>
+          </q-td>
+        </template>
         <template v-slot:body-cell-productOptionsLabel="props">
           <q-td :props="props">
             <div style="max-width: 250px; white-space: normal">
@@ -419,7 +428,7 @@ export default {
         //Request params
         let requestParams = {
           refresh: refresh,
-          params: {include: 'shippingDepartment,shippingCountry,customer.addresses,customer.fields'}
+          params: {include: 'shippingDepartment,shippingCountry,customer.addresses,customer.fields,orderItems.category'}
         }
         //Request
         this.$crud.show('apiRoutes.qcommerce.orders', this.orderId, requestParams).then(response => {
